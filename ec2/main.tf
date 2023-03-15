98b51cd83f9d9baa8052473e518e7f4de79a94da
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-15 16:39:33 +0000

    vgood

--- ec2/main.tf
+++ ec2/main.tf
@@ -19,7 +19,7 @@ resource "null_resource" "provisioner" {
         }
         
         inline = [
-          "ansible-pull -i localhost, -U https://github.com/Preetam6126/roboshop-ansible roboshop.yml -e role_name=${var.component}"   
+          "ansible-pull -i localhost, -U https://github.com/Preetam6126/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=$(var.env)"
         ]
         #below set is during terraform practice with hard code password
         # inline 
