b2873f21b55b1f528d7f2a6ebb82530e5e957b35
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-15 08:00:07 +0000

    good

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ module "ec2" {
     source = "./ec2"
     component = each.value["name"]
     instance_type = each.value["type"]
+    env           = var.env
     ## below password is used during terraform practice but we manage passwords with ansible 
   ##  password = try(each.value["password"], "null") 
   
-    env = var.env
+    
 }
 
