1e0ee7532c4eb4350fd16fe7532574c9a136758c
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-05 09:48:44 +0000

    all

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "docdb" {
   engine                     = each.value["engine"]
   engine_version             = each.value["engine_version"]
   backup_retention_period    = each.value["backup_retention_period"]
-  preferred_backup_windowe   = each.value["preferred_backup_window"]
+  preferred_backup_window    = each.value["preferred_backup_window"]
   skip_final_snapshot        = each.value["skip_final_snapshot"]
   subnet_ids                 = local.db_subnets_ids
 
