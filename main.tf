bb60c81d40e77847cd8e1af62a10172636515a3a
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 16:52:30 +0000

    hello

--- main.tf
+++ main.tf
@@ -75,18 +75,18 @@ module "vpc" {
 #   instance_type           = each.value["instance_type"]
 # }
 
-# module "alb" {  
+module "alb" {  
 
-#     source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
-#     env      = var.env
-#     tags     = var.tags
+    source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
+    env      = var.env
+    tags     = var.tags
     
-#     for_each           = var.alb
-#     name               = each.value["name"]
-#     internal           = each.value["internal"]
-#     load_balancer_type = each.value["load_balancer_type"]
-#     subnets            = lookup(local.subnet_ids,each.value["subnet_name"],null)     
-# }
+    for_each           = var.alb
+    name               = each.value["name"]
+    internal           = each.value["internal"]
+    load_balancer_type = each.value["load_balancer_type"]
+    subnets            = lookup(local.subnet_ids,each.value["subnet_name"],null)     
+}
 
 module "app" {  
 
