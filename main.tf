32a7502ea7aae012e0b163663f3ac38e7f0aee3b
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 16:58:46 +0000

    hello

--- main.tf
+++ main.tf
@@ -75,30 +75,30 @@ module "vpc" {
 #   instance_type           = each.value["instance_type"]
 # }
 
-module "alb" {  
+# module "alb" {  
 
-    source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
-    env      = var.env
-    tags     = var.tags
+#     source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
+#     env      = var.env
+#     tags     = var.tags
     
-    for_each           = var.alb
-    name               = each.value["name"]
-    internal           = each.value["internal"]
-    load_balancer_type = each.value["load_balancer_type"]
-    subnets            = lookup(local.subnet_ids,each.value["subnet_name"],null)     
-}
+#     for_each           = var.alb
+#     name               = each.value["name"]
+#     internal           = each.value["internal"]
+#     load_balancer_type = each.value["load_balancer_type"]
+#     subnets            = lookup(local.subnet_ids,each.value["subnet_name"],null)     
+# }
 
-module "app" {  
+# module "app" {  
 
-    source   = "git::https://github.com/Preetam6126/tf-module-app.git"
-    env      = var.env
-    tags     = var.tags 
+#     source   = "git::https://github.com/Preetam6126/tf-module-app.git"
+#     env      = var.env
+#     tags     = var.tags 
     
-    for_each      = var.app
-    component     = each.value["component"]
-    instance_type = each.value["instance_type"]
+#     for_each      = var.app
+#     component     = each.value["component"]
+#     instance_type = each.value["instance_type"]
   
-  }
+#   }
 # output "vpc" {
 #   value = module.vpc
 # }
