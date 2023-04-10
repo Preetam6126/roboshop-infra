e128e6c401cd2ba59c98d90c65baf8f215bff8ee
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-10 14:37:08 +0000

    yes

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-# module "vpc" {
+module "vpc" {
 
-#   source              = "git::https://github.com/Preetam6126/tf-module-vpc.git"
-#   env                 = var.env
-#   tags                = var.tags
-#   default_route_table = var.default_route_table
-#   default_vpc_id      = var.default_vpc_id
+  source              = "git::https://github.com/Preetam6126/tf-module-vpc.git"
+  env                 = var.env
+  tags                = var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id      = var.default_vpc_id
   
-#   for_each        = var.vpc 
-#   vpc_cidr        = each.value["vpc_cidr"]
-#   public_subnets  = each.value["public_subnets"]
-#   private_subnets = each.value["private_subnets"]
-#   }
+  for_each        = var.vpc 
+  vpc_cidr        = each.value["vpc_cidr"]
+  public_subnets  = each.value["public_subnets"]
+  private_subnets = each.value["private_subnets"]
+  }
 
 # module "docdb" {    
 
@@ -125,7 +125,7 @@
 #   value = module.vpc
 # }
 
-# output "vpc" {
-#   value = local.db_subnet_ids
-# }
+output "vpc" {
+  value = local.db_subnet_ids
+}
 
