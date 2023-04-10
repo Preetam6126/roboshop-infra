71b501a3f21aaa28ff42967694fece6ba0644982
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-10 14:34:55 +0000

    yes

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-module "vpc" {
+# module "vpc" {
 
-  source              = "git::https://github.com/Preetam6126/tf-module-vpc.git"
-  env                 = var.env
-  tags                = var.tags
-  default_route_table = var.default_route_table
-  default_vpc_id      = var.default_vpc_id
+#   source              = "git::https://github.com/Preetam6126/tf-module-vpc.git"
+#   env                 = var.env
+#   tags                = var.tags
+#   default_route_table = var.default_route_table
+#   default_vpc_id      = var.default_vpc_id
   
-  for_each        = var.vpc 
-  vpc_cidr        = each.value["vpc_cidr"]
-  public_subnets  = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
-  }
+#   for_each        = var.vpc 
+#   vpc_cidr        = each.value["vpc_cidr"]
+#   public_subnets  = each.value["public_subnets"]
+#   private_subnets = each.value["private_subnets"]
+#   }
 
 # module "docdb" {    
 
@@ -75,49 +75,50 @@ module "vpc" {
 #   instance_type           = each.value["instance_type"]
 # }
 
-module "alb" {  
+# module "alb" {  
 
-    source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
-    env      = var.env
-    tags     = var.tags
-      vpc_id  = module.vpc["main"].vpc_id
-    for_each           = var.alb
-    name               = each.value["name"]
-    internal           = each.value["internal"]
-    load_balancer_type = each.value["load_balancer_type"]
-    subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)  
-    allow_cidr         = each.value["allow_cidr"]
+#     source   = "git::https://github.com/Preetam6126/tf-module-alb.git"
+#     env      = var.env
+#     tags     = var.tags
+#       vpc_id  = module.vpc["main"].vpc_id
+#     for_each           = var.alb
+#     name               = each.value["name"]
+#     internal           = each.value["internal"]
+#     load_balancer_type = each.value["load_balancer_type"]
+#     subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)  
+#     allow_cidr         = each.value["allow_cidr"]
     
-}
+#     vpc_id = module.vpc["main"].vpc_id
+# }
 
-module "app" {  
-    source   = "git::https://github.com/Preetam6126/tf-module-app.git"
-    env           = var.env
-    tags          = var.tags 
-    bastion_cidr  = var.bastion_cidr
-    dns_domain    = var.dns_domain
+# module "app" {  
+#     source   = "git::https://github.com/Preetam6126/tf-module-app.git"
+#     env           = var.env
+#     tags          = var.tags 
+#     bastion_cidr  = var.bastion_cidr
+#     dns_domain    = var.dns_domain
     
-    vpc_id        = module.vpc["main"].vpc_id
+#     vpc_id        = module.vpc["main"].vpc_id
     
     
-    for_each      = var.apps
-    component        = each.value["component"]
-    instance_type    = each.value["instance_type"]
-    desired_capacity = each.value["desired_capacity"]
-    max_size         = each.value["max_size"]
-    min_size         = each.value["min_size"]
-    port             = each.value["port"]
-    listener_priority= each.value["listener_priority"]
-    subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
-    allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
-    alb_dns_name     = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null), "dns_name", null)
-    listener_arn     = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null), "arn", null)
+#     for_each      = var.apps
+#     component        = each.value["component"]
+#     instance_type    = each.value["instance_type"]
+#     desired_capacity = each.value["desired_capacity"]
+#     max_size         = each.value["max_size"]
+#     min_size         = each.value["min_size"]
+#     port             = each.value["port"]
+#     listener_priority= each.value["listener_priority"]
+#     subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
+#     allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
+#     alb_dns_name     = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null), "dns_name", null)
+#     listener_arn     = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null), "arn", null)
     
-  }
+#   }
   
-  output "alb" {
-    value = module.alb
-  }
+#   output "alb" {
+#     value = module.alb
+#   }
   
   
 # output "vpc" {
