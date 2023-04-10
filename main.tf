984721b77cccd4e4de31be7630676a86fc299044
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-10 14:45:28 +0000

    yes

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "vpc" {
 
   source              = "git::https://github.com/Preetam6126/tf-module-vpc.git"
-  env                 = var.env
-  tags                = var.tags
-  default_route_table = var.default_route_table
-  default_vpc_id      = var.default_vpc_id
+  # env                 = var.env
+  # tags                = var.tags
+  # default_route_table = var.default_route_table
+  # default_vpc_id      = var.default_vpc_id
   
-  for_each        = var.vpc 
-  vpc_cidr        = each.value["vpc_cidr"]
-  public_subnets  = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
+  # for_each        = var.vpc 
+  # vpc_cidr        = each.value["vpc_cidr"]
+  # public_subnets  = each.value["public_subnets"]
+  # private_subnets = each.value["private_subnets"]
   }
 
 # module "docdb" {    
