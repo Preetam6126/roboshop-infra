8ec0f1fd7539943634a2a71e48efc04df405c535
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-20 06:16:47 +0000

    thurs20

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_spot_instance_request" "load-runner" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.medium"
   wait_for_fulfillment = true
-  vpc_security_group_ids = ["allow_all"]
+  vpc_security_group_ids = ["allow-all"]
 
   tags   = merge(
     var.tags,
