1addbe50a6130535fcd790a2327178a5eb1f836e
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-20 05:55:26 +0000

    thurs20

--- main.tf
+++ main.tf
@@ -181,8 +181,8 @@ resource "aws_security_group" "loadrunner" {
   }
   
   egress {
-    description      = 0
     from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
