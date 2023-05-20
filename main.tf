3eec532993afc352cae21234bf5c1302c78382e6
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-20 14:24:10 +0000

    status

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
   }
 
+/*
 module "docdb" {    
 
   source   = "git::https://github.com/Preetam6126/tf-module-docdb.git"
@@ -184,3 +185,35 @@ output "vpc" {
 #     ]
 #   }
 # }
+*/
+
+  module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region        = "us-east-1"
+  cluster_name      = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key    = "~/.ssh/id_rsa.pub"
+  aws_subnet_id     = lookup(local.subnet_ids, "public", null)[0]
+  hosted_zone       = "Z099042325KVJ8P6CC8JI"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+  
+   addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+  
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+
+output "KUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+}
